4598e6addcd3df32819a2d6588d0f989ca517750
0xsambit <177391633+0xsambit@users.noreply.github.com>
2025-05-30 13:59:29 +0530

    Initialize Rust project with Cargo configuration, add .gitignore, and implement main functionality

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use colored::*;
+use std::io;
+fn main() {
+    println!("{}", "UNIKIT".bold().green());
+    println!(
+        "{}",
+        "The tool you need when you like linux, but stuck with Windows"
+            .bold()
+            .yellow()
+    );
+    println!();
+}
